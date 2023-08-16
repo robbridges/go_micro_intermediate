1c634f0d951688552c08dad97e140c6409b38add
Rob Bridges <Rmb101187@gmail.com>
2023-08-15 20:55:34 -0700

    Setting up broker with fake routes

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type App struct {
+}
+
 func main() {
+	app := App{}
+
+	log.Println("Starting broker service of port %s", webPort)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 
 }
