23151f6e0974b64781c888cd48e019186e4f0bb1
Rob Bridges <Rmb101187@gmail.com>
2023-08-23 16:48:14 -0700

    Adding rabbit MQ

---
+++ listener-service/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// try to connect to rabbit mq
+
+	// start listening for incoming messages/jobs
+
+	// create a consumer
+
+	// watch the queue && consume events
+
+}
