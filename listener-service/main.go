5f004f4b4cfc055913e86f4668ab8ed01040a6d1
Rob Bridges <Rmb101187@gmail.com>
2023-08-23 16:56:53 -0700

    Rabbit mq connection and potential back off routine impl

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,12 +1,59 @@
 package main
 
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"math"
+	"os"
+	"time"
+)
+
 func main() {
 	// try to connect to rabbit mq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 
+	defer rabbitConn.Close()
+	log.Println("connected to rabbit mq")
 	// start listening for incoming messages/jobs
 
 	// create a consumer
 
 	// watch the queue && consume events
 
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing off...")
+
+		time.Sleep(backOff)
+
+	}
+
+	return connection, nil
 }
