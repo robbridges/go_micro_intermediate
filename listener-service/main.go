422b56b78f87319fbbdced6e7237fa9f090f2f7b
Rob Bridges <Rmb101187@gmail.com>
2023-08-24 11:02:37 -0700

    consumer and events rabbit mq completed, connection complete, need to actually generate logs

--- listener-service/main.go
+++ listener-service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"listener/event"
 	"log"
 	"math"
 	"os"
@@ -18,13 +19,20 @@ func main() {
 	}
 
 	defer rabbitConn.Close()
-	log.Println("connected to rabbit mq")
 	// start listening for incoming messages/jobs
-
+	log.Println("listening for and consuming RabbitMQ messages")
 	// create a consumer
 
-	// watch the queue && consume events
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
 
+	// watch the queue && consume events
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func connect() (*amqp.Connection, error) {
@@ -39,6 +47,7 @@ func connect() (*amqp.Connection, error) {
 			fmt.Println("RabbitMQ not yet ready...")
 			counts++
 		} else {
+			log.Println("Connected to Rabbit MQ")
 			connection = c
 			break
 		}
